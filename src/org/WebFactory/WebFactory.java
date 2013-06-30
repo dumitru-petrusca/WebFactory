c77c3514d6d3a1013e430669fe4f2b7908a64776
AlexSoft <petruscaalex@MacBookPro.local>
2013-06-30 13:53:03 +0300

    Add WebFactory class

--- src/org/WebFactory/WebFactory.java
+++ src/org/WebFactory/WebFactory.java
@@ -1,7 +1,8 @@
-//import ....
+package org.WebFactory;
 
+class WebFactory {
 
-
-public static void main(Stringargs[]) {
-
-} 
+	public static void main(String args[]) {
+	    System.out.println("hello from WebFactory");
+        }
+}
